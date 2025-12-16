a07af810e2dc5c366d39c2b220399d29b763b651
dongyoon7212 <dongyoon7212@naver.com>
2025-12-16 10:26:13 +0900

    Feat: js

--- js_study/callback.js
+++ js_study/callback.js
@@ -4,16 +4,16 @@
 //비동기 콜백
 //비동기 작업이 완료된 후에 호출되는 콜백함수
 
-// function getData(callback) {
-//     setTimeout(() => {
-//         console.log("서버에서 데이터 받아옴");
-//         callback({ name: "이동윤" });
-//     }, 2000);
-// }
+function getData(callback) {
+    setTimeout(() => {
+        console.log("서버에서 데이터 받아옴");
+        callback({ name: "이동윤" });
+    }, 2000);
+}
 
-// getData((data) => {
-//     console.log(data);
-// });
+getData((data) => {
+    console.log(data);
+});
 
 //쇼핑몰
 //로그인
@@ -41,7 +41,7 @@ login("이동윤", (username) => {
     console.log(`${username}님이 로그인했습니다.`);
     addToCart("닌텐도", (product) => {
         console.log(`${product}가 장바구니에 추가되었습니다.`);
-        checkout("1234-5678-9012-3456", product, (cardNumber, product) => {
+        checkout("1234-56-789-5123", product, (cardNumber, product) => {
             console.log(
                 `${product}에 대한 결제가 완료되었습니다. 카드번호: ${cardNumber}`
             );
